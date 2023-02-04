05f1370b5f0a9fa7fa38e7b505af18a9a99a02da
YaroslavWeb <yaroslavweb98@gmail.com>
2023-02-04 19:24:46 +0800

    update container styles

--- src/pages/Lobby/styles.ts
+++ src/pages/Lobby/styles.ts
@@ -12,5 +12,7 @@ export const Container = styled.div`
   position: relative;
   z-index: ${({ theme }) => theme.order.content};
 
+  max-width: 1268px;
+  margin: 0 auto;
   padding-top: ${({ theme }) => theme.sizes.header.height}px;
 `
